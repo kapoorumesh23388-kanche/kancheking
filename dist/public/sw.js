dcb79ea44ecb79827f22e1a290ee027a69ba3d7f
kapoorumesh23388-kanche <kapoor.umesh23388@gmail.com>
2026-06-29 21:13:39 +0530

    fix: fresh build - tournament points 2500 fixed

---
+++ dist/public/sw.js
@@ -0,0 +1,4 @@
+// SW v3 
+self.addEventListener('install', e => self.skipWaiting()); 
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim())); }); 
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); }); 
